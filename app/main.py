ddabb635bc22bdfc5b4231178e8414db657f3e2a
Daniel Ryaboshapka <dan4821@gmail.com>
2020-06-22 13:17:44 -0400

    testing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from template import 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-	return ("<h1> Test Home Page </h1>")
+	return render_template('base.html', title='Home')
+
+@app.route("/api-calls1")
+def part_studio_contents():
+	return ("<h1> This Page Will Give You Part Studio Details</h1>")
+
+@app.route("/api-calls2")
+def feature_studio_contents():
+	return ("<h1> This Page Will Return Feature Studio Scripts</h1>")
+
+# @app.route("/api-calls3")
+# def 
