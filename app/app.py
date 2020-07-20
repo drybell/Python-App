c60b1acc3f9d48d151be059d74bee42650ee7660
Daniel Ryaboshapka <dan4821@gmail.com>
2020-07-20 13:59:21 -0400

    modified for heroku

--- app/app.py
+++ app/app.py
@@ -100,4 +100,4 @@ def send_uploaded_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0:8000")
+	app.run(host="0.0.0.0", port=8000)
